3dd1dafa567a9abd47fd75392fc52c49208dc1e6
unknown <siamur01@gmail.com>
2021-10-04 17:39:20 +0600

    m

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -16,7 +16,7 @@ const Services = () => {
 
    return (
       <div className="service-container">
-         <h1>Our Services</h1>
+         <h1><b>Our Courses</b></h1>
          <div className="services">
             <div className="row">
                {services.map(service => <div
